1cb89f3355669b7359b2d49f71067f17ab56fc72
Benjamin <benjonah25@gmail.com>
2026-01-07 14:10:50 -0700

    Add base path for subdirectory hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://git-hub-benjamin.github.io/bloggyblog',
+  base: '/bloggyblog',
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
